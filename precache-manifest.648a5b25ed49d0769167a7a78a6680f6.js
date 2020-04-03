b755f8f5ebefd47bd40f04bae1ca7d9bb68da350
sagoll <sagoll@iu.edu>
2020-04-03 04:45:08 -0400

    Updates

---
+++ precache-manifest.648a5b25ed49d0769167a7a78a6680f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3edc0c83e2268bfa9be2dfe46d34b7",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "3541e60792817e21dc6d",
+    "url": "/my_portfolio/static/css/main.91b47062.chunk.css"
+  },
+  {
+    "revision": "37979804768bae8af1fe",
+    "url": "/my_portfolio/static/js/2.4e08edc4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my_portfolio/static/js/2.4e08edc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3541e60792817e21dc6d",
+    "url": "/my_portfolio/static/js/main.b8f8ef39.chunk.js"
+  },
+  {
+    "revision": "6a0b52be57d4497e4ca8",
+    "url": "/my_portfolio/static/js/runtime-main.4adc97e7.js"
+  },
+  {
+    "revision": "01ca909d852ee02796ecb9d45e01fbd2",
+    "url": "/my_portfolio/static/media/logo.01ca909d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9b92e275f845d2b1fa3a610d8c566ef1.js"
+  "/my_portfolio/precache-manifest.648a5b25ed49d0769167a7a78a6680f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my_portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
